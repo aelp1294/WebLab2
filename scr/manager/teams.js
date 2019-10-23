9df667cb9c346b004e6f2f6a227c5b285b3be06e
Alvaro Leon <lepasepa_18@hotmail.com>
2019-10-23 16:29:53 -0600

    get function added

--- scr/manager/teams.js
+++ scr/manager/teams.js
@@ -1,9 +1,12 @@
-var teams = require('../../routes/localStorage');
+var teams = require('../../connectionToDB');
 
 /* funcion get cuando no hay un id */
+
 const GetAllItems = (req, res, next) => {
-    res.status(200)
-    res.json(teams)
+    teams.find({}, function(err, team) {      
+        res.status(200)
+        res.json(team)
+      });
 };
 
 /* funcion get cuando hay un id */
@@ -22,14 +25,29 @@ const GetUniqueItem = (req, res, next) => {
 /* funcion post para insertar un nuevo item */
 const AddNewItem = (req,res,next) => {
     const {body} = req
+    //(teams.find(item => item.id == body.id) == null) && (teams.find(item => item.nombre == body.nombre) == null) && 
     /*insertar unicamente si el id y el nombre no se repiten*/
-    if((teams.find(item => item.id == body.id) == null) && (teams.find(item => item.nombre == body.nombre) == null) && body.id != null && body.id > 0 && body.nombre != "" && body.liga != "" && body.campeonatos > 0 && body.puntos >= 0 && body.escudo != ""){
-        teams.push(body)
-        res.status(201)
-        res.send("agregado")   
-    } else {
-        res.status(404)
-        res.send("404 manejado")
+    if(body.id != null && body.id > 0 && body.nombre != "" && body.liga != "" && body.campeonatos > 0 && body.puntos >= 0 && body.escudo != ""){
+
+        // create a new user
+        var newTeam = team({
+            id: body.id,
+            liga: body.liga,
+            nombre: body.nombre,
+            campeonatos: body.campeonatos,
+            puntos: body.puntos,
+            escudo: body.escudo
+        });
+        newTeam.save(function(err){
+            if(err){
+                res.status(404)
+                res.send("404 manejado")
+            } else {
+                res.status(201)
+                res.send("agregado") 
+            }
+        });
+  
     }
 };
 
