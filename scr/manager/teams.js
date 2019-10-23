0172278d09c95707a10aff20e170bc8d192504b8
Alvaro Leon <lepasepa_18@hotmail.com>
2019-10-23 17:09:44 -0600

    other functions added

--- scr/manager/teams.js
+++ scr/manager/teams.js
@@ -12,22 +12,23 @@ const GetAllItems = (req, res, next) => {
 /* funcion get cuando hay un id */
 const GetUniqueItem = (req, res, next) => {
     /* revisar si el id existe, si si existe devolver el contenido del id*/
-    if(teams.find(item => item.id == req.params.id) != null){
-        res.status(200)
-        res.json(teams.find(item => item.id == req.params.id))
-    }
-    else{
-        res.status(404)
-        res.send("404 manejado")
-    }    
+    teams.findById(req.params.id, function(err, team) {
+        if(err){
+            res.status(404)
+            res.send("404 manejado")
+        } else{
+            res.status(200)
+            res.json(teams.find(item => item.id == req.params.id))
+        }
+    });
 };
 
 /* funcion post para insertar un nuevo item */
 const AddNewItem = (req,res,next) => {
     const {body} = req
-    //(teams.find(item => item.id == body.id) == null) && (teams.find(item => item.nombre == body.nombre) == null) && 
+    // Agregar validacion para cuando no hay nada en la tabla?
     /*insertar unicamente si el id y el nombre no se repiten*/
-    if(body.id != null && body.id > 0 && body.nombre != "" && body.liga != "" && body.campeonatos > 0 && body.puntos >= 0 && body.escudo != ""){
+    if((teams.find(item => item.id == body.id) == null) && (teams.find(item => item.nombre == body.nombre) == null) &&body.id != null && body.id > 0 && body.nombre != "" && body.liga != "" && body.campeonatos > 0 && body.puntos >= 0 && body.escudo != ""){
 
         // create a new user
         var newTeam = team({
@@ -48,43 +49,60 @@ const AddNewItem = (req,res,next) => {
             }
         });
   
+    } else {
+        res.status(404)
+        res.send("404 manejado")
     }
 };
 
 /* funcion put para actualizar un item */
 const UpdateItem = (req, res, next) => {
     const { body } = req
-    var object = teams.find(item => item.id == req.params.id);
+    var object = teams.findById(req.params.id, function(err, team){
         /*actualizar unicamente si los campos son validos*/
-    if(object != null && (teams.find(item => item.nombre == body.nombre) == null) && body.nombre != "" && body.liga != "" && body.campeonatos > 0 && body.puntos >= 0 && body.escudo != ""){
-        var i = teams.indexOf(object)
-        teams[i].liga = body.liga;
-        teams[i].nombre = body.nombre;
-        teams[i].campeonatos = body.campeonatos;
-        teams[i].puntos = body.puntos;
-        teams[i].escudo = body.escudo;
-        res.status(204)
-        res.send("actualizado")
-    }
-    else{
-        res.status(404)
-        res.send("404 manejado")
-    }
+        if(team != null && (teams.find(item => item.nombre == body.nombre) == null) && body.nombre != "" && body.liga != "" && body.campeonatos > 0 && body.puntos >= 0 && body.escudo != ""){
+            var i = teams.indexOf(object)
+            team.liga = body.liga;
+            team.nombre = body.nombre;
+            team.campeonatos = body.campeonatos;
+            team.puntos = body.puntos;
+            team.escudo = body.escudo;
+            team.save(function(err){
+                if(err){
+                    res.status(404)
+                    res.send("404 manejado")
+                } else {
+                    res.status(204)
+                    res.send("actualizado") 
+                }
+            });
+        }
+        else{
+            res.status(404)
+            res.send("404 manejado")
+        }
+    });
 };
 
 /* funcion delete para eliminar un item */
 const DeleteItem = (req, res, next) => {
     /* tratar de encontrar primero el id en la lista de items */
-    if(teams.find(item => item.id == req.params.id) != null){
-        var i = teams.indexOf(teams.find(item => item.id == req.params.id))
-        teams.splice(i, 1)
-        res.status(204)
-        res.send("borrado")
-    }
-    else{
-        res.status(404)
-        res.send("404 manejado")
-    }   
+    teams.findById(req.params.id, function(err, team) {
+        if(err){
+            res.status(404)
+            res.send("404 manejado")
+        } else{
+            team.remove(function(err) {
+                if (err){
+                    res.status(404)
+                    res.send("404 manejado")
+                } else {
+                    res.status(204)
+                    res.send("borrado")
+                }
+            });
+        }
+    });
 };
 
 module.exports = {
